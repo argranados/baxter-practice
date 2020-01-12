4e732318bd10f768ee7963b73d313fdd0c10e3df
argranados <argranados@yahoo.com>
2020-01-12 14:02:04 -0600

    app module cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PrcaticeOneComponent } from './prcatice-one/prcatice-one.component';
-import { PrcaticeTwoComponent } from './prcatice-two/prcatice-two.component';
+import { PrcaticeOneComponent } from './practice-one/prcatice-one.component';
+import { PrcaticeTwoComponent } from './practice-two/prcatice-two.component';
 import { PracticeThreeComponent } from './practice-three/practice-three.component';
 import { PracticeFourComponent } from './practice-four/practice-four.component';
 
