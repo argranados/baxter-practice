e2067625077f19a850ef58a041da8ec688374d4a
argranados <argranados@yahoo.com>
2020-01-12 18:59:40 -0600

    practice two

--- src/app/practice-two/prcatice-two.component.ts
+++ src/app/practice-two/prcatice-two.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+//import { DOCUMENT } from '@angular/platform-browser';
+import { Directive, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-prcatice-two',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prcatice-two.component.css']
 })
 export class PrcaticeTwoComponent implements OnInit {
+  constructor(@Inject(DOCUMENT) private document: any, private renderer: Renderer2) {}
 
-  constructor() { }
+  //constructor() { }
 
   ngOnInit() {
   }
+
+  firstFunction() {    
+    console.log("error");
+    var element = this.document.getElementById('txtbx');
+    element.parentNode.removeChild(element);
+    alert( 'Hello ' + '\nWelcome to C# Corner \nFunction in First Component');    
+  }   
+
+  addElementFunction() {
+    console.log("Adding element");
+    const inputTxt = this.renderer.createElement("input");
+    const padre = this.renderer.parentNode;
+    this.renderer.appendChild(padre,inputTxt);
+  }
+  
 
 }
