8df7ec211dab461412aabaf58809f204e73741cb
argranados <argranados@yahoo.com>
2020-01-12 18:41:08 -0600

    practice three inicial

--- src/app/practice-three/practice-three.component.ts
+++ src/app/practice-three/practice-three.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-practice-three',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practice-three.component.css']
 })
 export class PracticeThreeComponent implements OnInit {
+  file:any;
+  dataString:any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getFile(ev){
+    this.file = ev.target.files[0];
+  }
+
+  onFileChange() {
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+    //const file = ev.target.files[0];    
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        return initial;
+      }, {});
+      this.dataString = JSON.stringify(jsonData);
+      // document.getElementById('output').innerHTML = dataString.slice(0, 300).concat("...");
+      console.log(this.dataString);
+    }
+    reader.readAsBinaryString(this.file);
   }
 
 }
