4d1de5157f91bf0141906b682fd80e9e7208761b
titoadrianvelazquez@hotmail.com <titoadrianvelazquez@hotmail.com>
2020-01-12 18:46:21 -0600

    cambios en practica 4

--- src/app/practice-four/practice-four.component.ts
+++ src/app/practice-four/practice-four.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import printJS from 'print-js';
+import * as XLSX from 'xlsx'; 
 
 @Component({
   selector: 'app-practice-four',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PracticeFourComponent implements OnInit {
 
+  fileName= 'ExcelSheet.xlsx';  
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  exportPdf() {
+    printJS({
+      printable: 'table',
+      type: 'html',
+      scanStyles: true,
+      header: 'Practice Four table export'
+    });
+  }
+
+  exportExcel() {
+     /* table id is passed over here */   
+     let element = document.getElementById('table'); 
+     const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+     /* generate workbook and add the worksheet */
+     const wb: XLSX.WorkBook = XLSX.utils.book_new();
+     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+     /* save to file */
+     XLSX.writeFile(wb, this.fileName);
   }
 
 }
